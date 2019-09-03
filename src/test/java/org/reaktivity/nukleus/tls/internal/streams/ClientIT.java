db40849b5b16c2115d70b295f58ae4995e2cefcd
John Fallows <john.fallows@kaazing.com>
2019-09-02 18:00:07 -0700

    Update k3po dependencies

--- src/test/java/org/reaktivity/nukleus/tls/internal/streams/ClientIT.java
+++ src/test/java/org/reaktivity/nukleus/tls/internal/streams/ClientIT.java
@@ -49,7 +49,7 @@ public class ClientIT
             .clean();
 
     @Rule
-    public final TestRule chain = outerRule(timeout).around(reaktor).around(k3po);
+    public final TestRule chain = outerRule(reaktor).around(k3po).around(timeout);
 
     @Test
     @Specification({
