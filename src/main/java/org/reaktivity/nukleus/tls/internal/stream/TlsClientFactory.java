35e356f1ef41d2ef2c4214846e781140f83769b0
John Fallows <john.fallows@kaazing.com>
2020-08-25 14:14:18 -0700

    Support non-zero server initial padding and non-zero client reply padding

--- src/main/java/org/reaktivity/nukleus/tls/internal/stream/TlsClientFactory.java
+++ src/main/java/org/reaktivity/nukleus/tls/internal/stream/TlsClientFactory.java
@@ -599,7 +599,6 @@ public final class TlsClientFactory implements StreamFactory
             final TlsStream stream = client.stream.orElse(null);
             final int replyBudget = stream != null ? stream.replyBudget : 0;
             final int replyPadding = stream != null ? stream.replyPadding : 0;
-            assert replyPadding == 0;
 
             final int bytesOffset = tlsRecordInfo.sizeof();
             final int bytesConsumed = bytesOffset + tlsRecordInfo.length();
